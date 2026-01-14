b70492c1a3288c64a91924ac4abce892c99b8cc0
Erik van der Bas <erik@basedonline.nl>
2026-01-14 20:39:34 +0100

    chore: add rector

---
+++ rector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\StringableForToStringRector;
+use Rector\Php81\Rector\Array_\ArrayToFirstClassCallableRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+
+return RectorConfig::configure()
+   ->withPaths([
+      __DIR__ . '/rector.php',
+      __DIR__ . '/src',
+      __DIR__ . '/tests',
+   ])
+   ->withPhpSets(
+      php82: true,
+   )
+   ->withSkip([
+      ArrayToFirstClassCallableRector::class,
+      StringableForToStringRector::class,
+   ]);
